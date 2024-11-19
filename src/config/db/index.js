58f08055734c7e9197bea540702993ba840e5c7e
dainv <dainv@miagi-so.net>
2024-11-19 16:12:46 +0700

    fix typo

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,13 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 async function connect(params) {
-  try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/f8_edu_dev");
-    console.log("connected");
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/f8_edu_dev');
+        console.log('connected');
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export default { connect };
-ault { connect };
